eaac0272157bb60c9e51afd4b1ed6ee099974dcf
luisduenas <luis.duenas@sparktechs.com>
2019-03-08 11:57:01 -0800

    SERVCO-47: Lista de registros de checador

---
+++ src/client/src/actions/punchesActions.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const fetchPunchesData = userData => dispatch => {
+  axios
+    // .post("/api/test", userData)
+    .get("/api/test")
+    .then(res => {
+      console.log(res);
+      // Set current user
+      dispatch(setPunchesData(res));
+      dispatch({ type: SET_PUNCHES_DATA, payload: {} });
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+
+
+    
+};
+// Set logged in user
+export const setPunchesData = data => {
+  return {
+    type: SET_PUNCHES_DATA,
+    payload: data
+  };
+};
