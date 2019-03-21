b636dc28af7ff64154b741b6f2cbc8868fa6d654
luisduenas <luis.duenas@sparktechs.com>
2019-03-21 11:03:13 -0700

    fix warnings

--- src/client/src/components/common/IndicatorContainer.jsx
+++ src/client/src/components/common/IndicatorContainer.jsx
@@ -5,7 +5,7 @@ import clock from '@material-ui/icons/AccessTime'
 
 const IndicatorContainer = ({ indicators }) => {
   let content = [];
-  indicators.map((indicator, key) => {
+  indicators.forEach ((indicator, key) => {
     content.push(
       <div key={key} className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
         <InfoBox
